e5dd2e8a25fb2b747719e9007dd73c2d018123a6
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-09-03 16:37:00 -0300

    Window Cycling keybinds and clickable actions on XMobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,7 +7,7 @@ import XMonad.Util.SpawnOnce
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.EwmhDesktops (ewmh, fullscreenEventHook)
 
 import XMonad.Actions.CycleWS
 
@@ -37,7 +37,8 @@ myXMobarHook h = def
   { ppOutput = hPutStrLn h
   , ppCurrent = xmobarColor "yellow" ""
                 . wrap "[" "]"
-                . xmobarAddAction "xdotool key \\-\\-clearmodifiers Super_L+Tab"
+                . xmobarAddAction (Just 1) "xdotool key \\-\\-clearmodifiers Super_L+d"
+                . xmobarAddAction (Just 3) "xdotool key \\-\\-clearmodifiers Super_L+a"
   , ppVisible = xmobarColor "gray"   "" . wrap "(" ")"
   , ppUrgent  = xmobarColor "red"    "" . wrap ">" "<"
   , ppHidden  = hideString
@@ -61,13 +62,31 @@ myKeys =
 
   , ("M-S-l", safeSpawn "dm-tool" ["lock"])
 
+  -- Moving from Screen (for multiple monitors)
   , ("M-S-<Tab>", nextScreen)
 
+  -- Moving from Workspace
+  , ("M-d",       moveTo Next NonEmptyWS)
+  , ("M-<Right>", moveTo Next NonEmptyWS)
+
+  , ("M-a",      moveTo Prev NonEmptyWS)
+  , ("M-<Left>", moveTo Prev NonEmptyWS)
+
+  , ("M-S-d",       shiftToNext >> nextWS)
+  , ("M-S-<Right>", shiftToNext >> nextWS)
+
+  , ("M-S-a",      shiftToPrev >> prevWS)
+  , ("M-S-<Left>", shiftToPrev >> prevWS)
+
+  , ("M-n",        moveTo Next EmptyWS)
+
+  -- Screenshot
   , ("<Print>"    , safeSpawn "gnome-screenshot" [])
   , ("S-<Print>"  , safeSpawn "gnome-screenshot" ["--area"])
   , ("M-<Print>"  , safeSpawn "gnome-screenshot" ["--window"])
   , ("C-S-<Print>", safeSpawn "gnome-screenshot" ["--window"])
 
+  -- Audio control
 #ifdef XMonadExtras
   , ("<XF86AudioLowerVolume>", lowerVolume 3 >> return ())
   , ("M-S-<F2>"              , lowerVolume 3 >> return ())
@@ -79,6 +98,7 @@ myKeys =
   , ("M-S-<F4>"              , toggleMute    >> return ())
 #endif
 
+  -- Brightness control
   , ("<XF86MonBrightnessDown>", setBright (\x -> x - 50))
   , ("<XF86Mail>"             , setBright (\x -> x - 50))
   , ("M-S-<F9>"               , setBright (\x -> x - 50))
@@ -106,5 +126,6 @@ alterFile f h = do
 hideString :: String -> String
 hideString _ = ""
 
-xmobarAddAction :: String -> (String -> String)
-xmobarAddAction cmd = wrap ("<action=`" ++ cmd ++ "`>") "</action>"
+xmobarAddAction :: Maybe Int -> String -> (String -> String)
+xmobarAddAction Nothing cmd = wrap ("<action=`" ++ cmd ++ "`>") "</action>"
+xmobarAddAction (Just n) cmd = wrap ("<action=`" ++ cmd ++ "` button="++ (show n) ++">") "</action>"
